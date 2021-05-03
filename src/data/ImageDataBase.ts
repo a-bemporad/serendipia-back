1e3e3a84383c3eaf35a12c3381deb30694d5968d
astrid <astridbemporad@gmail.com>
2021-05-03 15:34:42 -0300

    Create Image model and ImageDataBase

---
+++ src/data/ImageDataBase.ts
@@ -0,0 +1,64 @@
+import { Image } from "../model/image";
+import BaseDataBase from "./BaseDataBase";
+
+export class ImageDataBase extends BaseDataBase {
+  protected tableName: string = "gallery_image";
+
+  private toModel(dbModel?: any): Image | undefined {
+    return (
+      dbModel &&
+      new Image(
+        dbModel.id,
+        dbModel.subtitle,
+        dbModel.author_id,
+        dbModel.date,
+        dbModel.file,
+        dbModel.tags,
+        dbModel.collection
+      )
+    );
+  }
+  public async createImage(image: Image): Promise<void> {
+    try {
+      await BaseDataBase.connection
+        .insert({
+          id: image.getId(),
+          subtitle: image.getSubtitle(),
+          author_id: image.getAuthorId(),
+          date: image.getDate(),
+          file: image.getFile(),
+          tags: image.getTags(),
+          collection: image.getCollection(),
+        })
+        .into(this.tableName);
+    } catch (error) {
+      throw new Error(error.sqlMessage || error.message);
+    }
+  }
+  public async getImageById(reqImageId: string): Promise<Image | undefined> {
+    try {
+      const result = await BaseDataBase.connection
+        .select("*")
+        .from(this.tableName)
+        .where({ id: reqImageId });
+
+      const image = this.toModel(result);
+      return image;
+    } catch (error) {
+      throw new Error(error.sqlMessage || error.message);
+    }
+  }
+  public async getAllImages(): Promise<Image[] | undefined> {
+    try {
+      const result: Image[] = await BaseDataBase.connection
+        .select("*")
+        .from(this.tableName);
+
+      return result;
+    } catch (error) {
+      throw new Error(error.sqlMessage || error.message);
+    }
+  }
+}
+
+export default new ImageDataBase();
